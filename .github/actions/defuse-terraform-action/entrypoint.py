3b279cb2bdc98b92ab57d9d3a3ece7039f12c000
Mikmocc00 <michele.moccia03@gmail.com>
2026-03-19 14:41:20 +0100

    Create entrypoint.py

---
+++ .github/actions/defuse-terraform-action/entrypoint.py
@@ -0,0 +1,59 @@
+#!/bin/python3
+
+import json
+import os
+import requests
+import sys
+from github import Github
+from ansiblemetrics.metrics_extractor import extract_all as extract_ansible_metrics
+from toscametrics.metrics_extractor import extract_all as extract_tosca_metrics
+from repominer.metrics.terraform import TerraformMetricsExtractor
+
+language = os.getenv('INPUT_LANGUAGE')
+g = Github(os.getenv('GITHUB_TOKEN'))
+repo = g.get_repo(os.getenv('GITHUB_REPOSITORY'))
+files = repo.get_commit(sha=os.getenv('GITHUB_SHA')).files
+
+# Inizializza l'estrattore Terraform
+tf_extractor = TerraformMetricsExtractor(path_to_repo='.', clone_repo_to='.', at='release') if language == 'terraform' else None
+
+for file in files:
+    # Se stiamo analizzando Terraform, ignoriamo i file che non sono .tf
+    if language == 'terraform' and not file.filename.endswith('.tf'):
+        continue
+
+    content = repo.get_contents(file.filename, ref=os.getenv('GITHUB_SHA')).decoded_content.decode()
+    metrics = {}
+
+    try:
+        if language == 'ansible':
+            metrics = extract_ansible_metrics(content)
+        elif language == 'tosca':
+            metrics = extract_tosca_metrics(content)
+        elif language == 'terraform':
+            metrics = tf_extractor.get_product_metrics(content)
+        else:
+            print('Filter Error! Skipping', file.filename)
+            sys.stdout.flush()
+            continue
+    except Exception as e:
+        print(f"⚠️ Errore di parsing in {file.filename}: {e}")
+        sys.stdout.flush()
+        metrics = {"syntax_error": 1}
+
+    url = f'{os.getenv("INPUT_URL")}/predict?model_id={os.getenv("INPUT_MODEL")}'
+    for name, value in metrics.items():
+        url += f'&{name}={value}'
+
+    headers = {"ngrok-skip-browser-warning": "true"}
+    try:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            response_content = json.loads(response.content.decode())
+            print(file.filename, ':', response_content)
+        else:
+            print('Response status:', response.status_code)
+    except Exception as e:
+        print(f"Errore durante la richiesta al modello: {e}")
+
+    sys.stdout.flush()
