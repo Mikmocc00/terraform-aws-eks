b7d26b24c5147dff0b73c522848e8940ad913295
Mikmocc00 <michele.moccia03@gmail.com>
2026-03-24 10:56:43 +0100

    Update test_defects.tf

--- test_defects.tf
+++ test_defects.tf
@@ -1,10 +1,8 @@
-# Risorsa per generare una password casualea
-# Questa configurazione serve per il database
-#ti dico che za
+
 resource "random_password" "password_sicura" {
   length  = 16
 
   special = true
 }
 
-# Fine del file
+# Fine del filedwdmopkdwm,dolomcoMCKDnVLkcndslkavnlkNDVINViNKLVNKLDNVlkSDNVIONBvoiNV'JNVj SJNSVnwJVOEMNVP
