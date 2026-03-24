520970a88061cf103d3390b80493e87c96381499
Mikmocc00 <michele.moccia03@gmail.com>
2026-03-24 11:35:20 +0100

    Update test_defects.tf

--- test_defects.tf
+++ test_defects.tf
@@ -1,6 +1,6 @@
 /**
  * # Risorsa Password Casualea
- * Questa risorsa genera una password sicura di 16 caratteri.
+ * Questa risorsa genera una paaassword sicura di 16 caratteri.
  * Viene utilizzata per garantire la sicurezza degli accessi iniziali.
  */
 resource "random_password" "password_sicura" {
