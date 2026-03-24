2daddbba4c46421b23235c6e646ce03aab954f32
Mikmocc00 <michele.moccia03@gmail.com>
2026-03-24 11:57:13 +0100

    Update test_defects.tf

--- test_defects.tf
+++ test_defects.tf
@@ -5,5 +5,5 @@
  */
 resource "random_password" "password_sicura" {
   length  = 16
-  special = true # Utilizzo di caratteri speciali per aumentare la robustezzai
+  special = true # Utilizzo di caratteri speciali per aumentare la robustezzaiaa
 }
