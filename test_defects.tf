2cf0b0fe1d5863111696b9158aa3d40e06a3e8d5
Mikmocc00 <michele.moccia03@gmail.com>
2026-04-28 17:40:44 +0200

    Fix typos in test_defects.tf comments and attributes

--- test_defects.tf
+++ test_defects.tf
@@ -1,9 +1,9 @@
 /**
  * # Risorsa Password Casualea
  * Questa risorsa genera una paaassword sicura di 16 caratteri.
- * Viene utilizzata per garantire la sicurezza degli accessi iniziali.dewfewfewfewfef
+ * Viene utilizzata per garantire la sicurezza degli accessi iniziali.dewfewfewfewfefs2s2s2
  */
 resource "random_password" "password_sicura" {
   length  = 16
-  special = true # Utilizzo di caratteri speciali per aumentare la robustezzaiaaa
+  special = true # Utilizzo di caratteri speciali per aumentare la robustezzaiaaassa2e2s
 }
